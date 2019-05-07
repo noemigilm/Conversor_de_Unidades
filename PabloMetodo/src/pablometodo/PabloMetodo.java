43d05bba5d1010c22dc8030b02090eb6e8483d13
Guard <pablolorenzoagrela@gmail.com>
2019-05-07 14:56:13 +0200

    Metodo Longitud acabado con menu incorporado

--- PabloMetodo/src/pablometodo/PabloMetodo.java
+++ PabloMetodo/src/pablometodo/PabloMetodo.java
@@ -5,40 +5,55 @@
  */
 package pablometodo;
 
-import sun.net.www.content.audio.x_aiff;
+import java.util.Scanner;
 
 /**
  *
  * @author DAM116
  */
 public class PabloMetodo {
-
+static Scanner leer = new Scanner(System.in);
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int x = 3;
-        
+        double x = 3;
         lonxitude(x);
         
     }
 
-    private static void lonxitude(int x) {
-        x = 0;
-        
-        switch (x) {
+    private static void lonxitude(double b) {
+        System.out.println("Seleccione un metodo de conversion");
+        System.out.println("1. Transformar Kms a Millas");
+        System.out.println("2. Transformar Millas a Kms");
+        System.out.println("3. Transformar Millas a Pulgadas");
+        System.out.println("4. Transformar Pulgadas a Millas");
+        System.out.println("0. Volver ó menú anterior");
+        int a= leer.nextInt();
+        switch (a) {
             case 1:
-                
-                
+                System.out.println("A seleccionado: KM a Millas");
+                b = b*0.621371;
+                System.out.println(b);
                 break;
             case 2:
+                System.out.println("A seleccionado: Millas a KM");
+                b = b*1.60934;
+                System.out.println(b);
                 break;
             case 3:
+                System.out.println("A seleccionado: KM a Millas");
+                b = b*63360;
+                System.out.println(b);
                 break;
             case 4:
+                System.out.println("A seleccionado: KM a Millas");
+                b = b/63360;
+                System.out.println(b);
                 break;
             case 0:
+                System.out.println("A seleccionado: Volver al anterior menu");
                 break;
             default:
                 break;
