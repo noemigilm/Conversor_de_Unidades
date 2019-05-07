a34e1f2ef392c4b3d1b1b492760dfbbe3a06a6e7
Gatosaurio <gatito931234@gmail.com>
2019-05-07 14:29:01 +0200

    interfaz conversor

--- src/conversor_de_unidades/Conversor_de_Unidades.java
+++ src/conversor_de_unidades/Conversor_de_Unidades.java
@@ -5,6 +5,9 @@
  */
 package conversor_de_unidades;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *
  * @author DAM110
@@ -16,11 +19,54 @@ public class Conversor_de_Unidades {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
-    public static double Lonxitude(){
-        int tipo;
+    
+    /* public static double Lonxitude(){
+       int tipo;
         double lonxitude=0;
-        return lonxitude;
+        return lonxitude;*/
+       
+   /* Scanner sc = new Scanner(System.in);*/
+    
+    
+       
+       Scanner sn = new Scanner(System.in);
+        boolean salir = false;
+        int opcion; //Guardaremos la opcion del usuario
+ 
+        while (!salir) {
+            System.out.println("Elige el tipo de conversion:");
+            System.out.println("1. lonxitude.");
+            System.out.println("2. Proximamente");
+            System.out.println("3. Salir");
+ 
+            try {
+ 
+                System.out.println("Escribe una de las opciones");
+                opcion = sn.nextInt();
+ 
+                switch (opcion) {
+                    case 1:
+                        System.out.println("Has seleccionado la opcion Conversion de Longitud");
+                        System.out.println("introduce los datos:");
+                        
+                        break;
+                    case 2:
+                        System.out.println("Has seleccionado la opcion 2");
+                        break;
+                    
+                    case 3:
+                        salir = true;
+                        break;
+                    default:
+                        System.out.println("Solo números entre 1 y 3");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Debes insertar un número");
+                sn.next();
+            }
+        }
+ 
+    
         
     }
 }
