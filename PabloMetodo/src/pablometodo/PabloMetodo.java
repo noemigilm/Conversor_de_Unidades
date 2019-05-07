151b15409d026e708c448a45db1597517eac6b87
Guard <pablolorenzoagrela@gmail.com>
2019-05-07 14:36:46 +0200

    en proceso de hacer el metodo Longitud

---
+++ PabloMetodo/src/pablometodo/PabloMetodo.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pablometodo;
+
+import sun.net.www.content.audio.x_aiff;
+
+/**
+ *
+ * @author DAM116
+ */
+public class PabloMetodo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x = 3;
+        
+        lonxitude(x);
+        
+    }
+
+    private static void lonxitude(int x) {
+        x = 0;
+        
+        switch (x) {
+            case 1:
+                
+                
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 0:
+                break;
+            default:
+                break;
+        }
+    }
+    
+    
+}
