6ea7ea6c3e7162b583e20137f7cd052b00e6b931
Guard <pablolorenzoagrela@gmail.com>
2019-05-16 08:56:53 +0200

    metodo Temperatura añadido

--- PabloMetodo/src/pablometodo/PabloMetodo.java
+++ PabloMetodo/src/pablometodo/PabloMetodo.java
@@ -65,7 +65,7 @@ static Scanner leer = new Scanner(System.in);
         System.out.println("1. Transformar WK a CV");
         System.out.println("2. Transformar CV a KW");
         System.out.println("3. Transformar KW a Ft/Lb");
-        System.out.println("4. Transformar Ft/lb a KW"); 
+        System.out.println("4. Transformar Ft/lb a KW");  
         System.out.println("0. Volver ó menú anterior");
         int a= leer.nextInt();
         switch (a) {
@@ -87,6 +87,42 @@ static Scanner leer = new Scanner(System.in);
             case 4:
                 System.out.println("A seleccionado: Ft/Lb a KW");
                 b = b*0.0013558179;
+                System.out.println(b);
+                break;
+            case 0:
+                System.out.println("A seleccionado: Volver al anterior menu");
+                break;
+            default:
+                break;
+        }
+    }
+    private static void Temperatura(double b) {
+        System.out.println("Seleccione un metodo de conversion");
+        System.out.println("1. Transformar Celsius a Fahrenheit");
+        System.out.println("2. Transformar Fahrenheit a Celsius");
+        System.out.println("3. Transformar Celsius a Kelvin");
+        System.out.println("4. Transformar Kelvin a Celsius");  
+        System.out.println("0. Volver ó menú anterior");
+        int a= leer.nextInt();
+        switch (a) {
+            case 1:
+                System.out.println("A seleccionado: Celsius a Fahrenheit");
+                b = (b*9 / 5) + 32;
+                System.out.println(b);
+                break;
+            case 2:
+                System.out.println("A seleccionado: Fahrenheit a Celsius");
+                b = (b-32)*5 / 9 ;
+                System.out.println(b);
+                break;
+            case 3:
+                System.out.println("A seleccionado:Celsius a Kelvin");
+                b = b+273.15;
+                System.out.println(b);
+                break;
+            case 4:
+                System.out.println("A seleccionado: Kelvin a Celsius");
+                b = b-273.15;
                 System.out.println(b);
                 break;
             case 0:
