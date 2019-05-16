ba85cf01a102dbff73bc9fde8e441365d41c7ad3
Noemi <noemi.gil.mu@gmail.com>
2019-05-16 09:47:11 +0200

    Potencia Y Temperatura Añadido

--- src/conversor_de_unidades/Conversor_de_Unidades.java
+++ src/conversor_de_unidades/Conversor_de_Unidades.java
@@ -1,9 +1,6 @@
-﻿/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package conversor_de_unidades;
+
+
 
 
 import java.util.InputMismatchException;
@@ -35,8 +32,9 @@ static Scanner leer = new Scanner(System.in);
         while (!salir) {
             System.out.println("Elige el tipo de conversion:");
             System.out.println("1. lonxitude.");
-            System.out.println("2. Proximamente");
-            System.out.println("3. Salir");
+            System.out.println("2. Potencia");
+            System.out.println("3. Temperatura");
+            System.out.println("4. Salir");
  
             try {
  
@@ -50,11 +48,15 @@ static Scanner leer = new Scanner(System.in);
                         lonxitude();
                         break;
                     case 2:
-                        System.out.println("Has seleccionado la opcion 2");
-                        
+                        System.out.println("Has seleccionado la opcion Potencia");
+                            Potencia();
+                        break;
+                    case 3:
+                        System.out.println("Has seleccionado la opcion Conversion Temperatura");
+                            Temperatura();
                         break;
                     
-                    case 3:
+                    case 4:
                         salir = true;
                         break;
                     default:
@@ -70,15 +72,15 @@ static Scanner leer = new Scanner(System.in);
         
     }
     private static int insertarNumero(){
-        System.out.println("Introduzca la longitud a transformar: ");
+        System.out.println("Introduzca el numero a transformar: ");
         /**
-         * @param longitud unidad a convertir
+         * @param numero unidad a convertir
          */
-        int longitud=leer.nextInt();
+        int numero=leer.nextInt();
         /**
          * @return devuelve la longitud
          */
-        return longitud;
+        return numero;
     }
     
     
@@ -129,76 +131,78 @@ static Scanner leer = new Scanner(System.in);
                 break;
         }
     }
-private static void Potencia(double b) {
-        System.out.println("Seleccione un metodo de conversion");
-        System.out.println("1. Transformar WK a CV");
-        System.out.println("2. Transformar CV a KW");
-        System.out.println("3. Transformar KW a Ft/Lb");
-        System.out.println("4. Transformar Ft/lb a KW");  
-        System.out.println("0. Volver ó menú anterior");
-        int a= leer.nextInt();
-        switch (a) {
-            case 1:
-                System.out.println("A seleccionado: KW a CV");
-                b = b*1.34102;
-                System.out.println(b);
-                break;
-            case 2:
-                System.out.println("A seleccionado: CV a KW");
-                b = b*0.7457;
-                System.out.println(b);
-                break;
-            case 3:
-                System.out.println("A seleccionado: KW a Ft/Lb");
-                b = b*737.562149;
-                System.out.println(b);
-                break;
-            case 4:
-                System.out.println("A seleccionado: Ft/Lb a KW");
-                b = b*0.0013558179;
-                System.out.println(b);
-                break;
-            case 0:
-                System.out.println("A seleccionado: Volver al anterior menu");
-                break;
-            default:
-                break;
+    private static void Potencia() {
+            double c=insertarNumero();
+            System.out.println("Seleccione un metodo de conversion");
+            System.out.println("1. Transformar WK a CV");
+            System.out.println("2. Transformar CV a KW");
+            System.out.println("3. Transformar KW a Ft/Lb");
+            System.out.println("4. Transformar Ft/lb a KW");  
+            System.out.println("0. Volver ó menú anterior");
+            int a= leer.nextInt();
+            switch (a) {
+                case 1:
+                    System.out.println("A seleccionado: KW a CV");
+                    c = c*1.34102;
+                    System.out.println(c);
+                    break;
+                case 2:
+                    System.out.println("A seleccionado: CV a KW");
+                    c = c*0.7457;
+                    System.out.println(c);
+                    break;
+                case 3:
+                    System.out.println("A seleccionado: KW a Ft/Lb");
+                    c = c*737.562149;
+                    System.out.println(c);
+                    break;
+                case 4:
+                    System.out.println("A seleccionado: Ft/Lb a KW");
+                    c = c*0.0013558179;
+                    System.out.println(c);
+                    break;
+                case 0:
+                    System.out.println("A seleccionado: Volver al anterior menu");
+                    break;
+                default:
+                    break;
+            }
         }
-    }
-    private static void Temperatura(double b) {
-        System.out.println("Seleccione un metodo de conversion");
-        System.out.println("1. Transformar Celsius a Fahrenheit");
-        System.out.println("2. Transformar Fahrenheit a Celsius");
-        System.out.println("3. Transformar Celsius a Kelvin");
-        System.out.println("4. Transformar Kelvin a Celsius");  
-        System.out.println("0. Volver ó menú anterior");
-        int a= leer.nextInt();
-        switch (a) {
-            case 1:
-                System.out.println("A seleccionado: Celsius a Fahrenheit");
-                b = (b*9 / 5) + 32;
-                System.out.println(b);
-                break;
-            case 2:
-                System.out.println("A seleccionado: Fahrenheit a Celsius");
-                b = (b-32)*5 / 9 ;
-                System.out.println(b);
-                break;
-            case 3:
-                System.out.println("A seleccionado:Celsius a Kelvin");
-                b = b+273.15;
-                System.out.println(b);
-                break;
-            case 4:
-                System.out.println("A seleccionado: Kelvin a Celsius");
-                b = b-273.15;
-                System.out.println(b);
-                break;
-            case 0:
-                System.out.println("A seleccionado: Volver al anterior menu");
-                break;
-            default:
-                break;
+        private static void Temperatura() {
+            double d=insertarNumero();
+            System.out.println("Seleccione un metodo de conversion");
+            System.out.println("1. Transformar Celsius a Fahrenheit");
+            System.out.println("2. Transformar Fahrenheit a Celsius");
+            System.out.println("3. Transformar Celsius a Kelvin");
+            System.out.println("4. Transformar Kelvin a Celsius");  
+            System.out.println("0. Volver ó menú anterior");
+            int a= leer.nextInt();
+            switch (a) {
+                case 1:
+                    System.out.println("A seleccionado: Celsius a Fahrenheit");
+                    d = (d*9 / 5) + 32;
+                    System.out.println(d);
+                    break;
+                case 2:
+                    System.out.println("A seleccionado: Fahrenheit a Celsius");
+                    d = (d-32)*5 / 9 ;
+                    System.out.println(d);
+                    break;
+                case 3:
+                    System.out.println("A seleccionado:Celsius a Kelvin");
+                    d = d+273.15;
+                    System.out.println(d);
+                    break;
+                case 4:
+                    System.out.println("A seleccionado: Kelvin a Celsius");
+                    d = d-273.15;
+                    System.out.println(d);
+                    break;
+                case 0:
+                    System.out.println("A seleccionado: Volver al anterior menu");
+                    break;
+                default:
+                    break;
+            }
         }
-    }
 }
